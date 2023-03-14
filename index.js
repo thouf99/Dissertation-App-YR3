ca4b0a7bb587463d6beb877bde2c0cbaf80f84e5
tchow006 <126357349+tchow006@users.noreply.github.com>
2023-03-14 20:29:15 +0000

    update index.js
    Getting rid of password information

--- index.js
+++ index.js
@@ -39,8 +39,7 @@ app.use(express.static(__dirname + '/public'));
 const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
-    // password: 'Chowdhury12',
-    password: 'Ch@wdhury12',
+    password: '*****',
     database: 'Wedding'
 });
 // Connect to the database
